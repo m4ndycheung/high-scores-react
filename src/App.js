d6dbb2780efaf4e124ce9f572fd57de22d275e2e
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-07-08 15:29:15 +0100

    Sorted country names

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import "./App.css";
 import HighScoreTable from "./HighScoreTable";
 import allCountryScores from "./scores";
 
+// single statement arrow functions do not need curly brackets.
+// after the arrow it will return.
+// if I add curly brackets, I need to write 'return'
 const App = () => {
-  return <HighScoreTable countries={allCountryScores} />;
+  let sortedCountryScores = allCountryScores.sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+  return <HighScoreTable countries={sortedCountryScores} />;
 };
 
 export default App;
